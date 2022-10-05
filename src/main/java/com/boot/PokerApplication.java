af314bdd8e4b0134b76904fed3b1ee693cc63176
Ame to Yuki <oban615@gmail.com>
2022-10-05 23:39:49 +0900

    poker class commit

--- src/main/java/com/boot/PokerApplication.java
+++ src/main/java/com/boot/PokerApplication.java
@@ -1,13 +1,16 @@
-package com.example.demo;
+package com.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.poker.Poker;
 
 @SpringBootApplication
 public class PokerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PokerApplication.class, args);
+		Poker.start();
 	}
 
 }
